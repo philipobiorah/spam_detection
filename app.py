e754009e65c0b8857c1efe04838c9debd2757b5c
philipobiorah <philip.obiorah@outlook.com>
2019-05-03 01:10:57 +0100

    Working app v1

--- app.py
+++ app.py
@@ -1,36 +1,54 @@
-from flask import Flask,render_template,url_for,request
+from flask import Flask, render_template, url_for, request
 import pandas as pd
 import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.externals import joblib
+
 app = Flask(__name__)
-@app.route('/', methods=['POST','GET'])
+
+
+@app.route('/')
+def home():
+   return render_template('home.html')
+
+
+@app.route('/predict', methods=['POST'])
 def predict():
-   if request.method == 'GET':
-      return render_template('index.html')
-   df= pd.read_csv("spam.csv", encoding="latin-1")
+   df = pd.read_csv("spam.csv", encoding="latin-1")
    df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1, inplace=True)
    # Features and Labels
-   df['label'] = df['v1'].map({'ham': 0, 'spam': 1})
-   df['message'] = df['v2']
+   df['label'] = df['class'].map({'ham': 0, 'spam': 1})
    X = df['message']
    y = df['label']
-   # Feature Extraction and Bow creation using CountVectorizer
-   bow_transformer = CountVectorizer()
-   X = bow_transformer.fit_transform(df['message'])
-   #Load the model that was created initially
-   spam_model = open('our_model.pkl','rb')
-   clf = joblib.load(spam_model)
+
+   # Extract Feature With CountVectorizer
+   cv = CountVectorizer()
+   X = cv.fit_transform(X)  # Fit the Data
+   from sklearn.model_selection import train_test_split
+   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+   # Naive Bayes Classifier
+   from sklearn.naive_bayes import MultinomialNB
+
+   clf = MultinomialNB()
+   clf.fit(X_train, y_train)
+   clf.score(X_test, y_test)
+   # Alternative Usage of Saved Model
+   # joblib.dump(clf, 'NB_spam_model.pkl')
+   # NB_spam_model = open('NB_spam_model.pkl','rb')
+   # clf = joblib.load(NB_spam_model)
+
    if request.method == 'POST':
-      message = request.form['sample_text']
+      message = request.form['message']
       data = [message]
-      vect = bow_transformer.transform(data).toarray()
+      vect = cv.transform(data).toarray()
       my_prediction = clf.predict(vect)
-      my_prediction = str(my_prediction[0])
-      if my_prediction == '1':
-         return "That looks like a spam message"
-      else:
-         return "This looks like a ham message."
-if __name__ == '__predict__':
+   return render_template('result.html', prediction=my_prediction)
+
+
+if __name__ == '__main__':
    app.run(debug=True)
+
+
+
+#df['label'] = df['v1'].map({'ham': 0, 'spam': 1})
